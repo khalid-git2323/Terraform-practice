68604c0c2cf8d47182545a4ad68fa5e220ec1ab6
root <root@ip-172-31-35-152.ec2.internal>
2025-08-07 18:53:15 +0000

    day-4

--- Day-4-statelocking/main.tf
+++ Day-4-statelocking/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "name" {
     ami = "ami-084a7d336e816906b"
     instance_type = "t3.micro"
   tags = {
-    name="dev-1"
+    name="dev-2"
   }
 }
 resource "aws_vpc" "name" {
