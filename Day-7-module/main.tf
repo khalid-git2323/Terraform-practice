7ff2bfd445d433236571c5492aeefba46be22255
khalid-git2323 <khalidjamadar100@gmail.com>
2025-08-26 15:13:19 +0530

    day-7-module

---
+++ Day-7-module/main.tf
@@ -0,0 +1,7 @@
+#module "ec2_instance" {
+ # source        = "../Day-7-sourceec2-module"
+ # ami_id        = "ami-01102c5e8ab69fb75"   # Tumhari AMI ID
+#  instance_type = "t3.micro"
+#  instance_name = "Demo-EC2"
+#}
+
